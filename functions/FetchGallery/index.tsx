31fb9cbba0ae8e9af696f562f6a6b40b9943bc34
stratroulettehub <p.lakis@icloud.com>
2021-09-05 13:02:37 +1000

    fix issue for opensea api limit

--- functions/FetchGallery/index.tsx
+++ functions/FetchGallery/index.tsx
@@ -51,7 +51,7 @@ export const FetchFromOpenSea = async (
   }
 
   try {
-    const URI = `https://api.opensea.io/api/v1/assets?&order_direction=asc&offset=0&limit=200&owner=${address}`;
+    const URI = `https://api.opensea.io/api/v1/assets?&order_direction=asc&offset=0&limit=50&owner=${address}`;
     const response = await axios.get(URI);
     const tokenArray = response.data.assets;
     const formattedTokens = formatTokens(tokenArray, address);
